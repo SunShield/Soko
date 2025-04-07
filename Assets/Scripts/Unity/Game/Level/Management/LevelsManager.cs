1333cc5e59377069f93151a3da26e57a6c6973b7
Herman <ustyanherman@gmail.com>
2025-04-07 08:11:41 +0300

    Last level bug fix

--- Assets/Scripts/Unity/Game/Level/Management/LevelsManager.cs
+++ Assets/Scripts/Unity/Game/Level/Management/LevelsManager.cs
@@ -24,12 +24,13 @@ namespace Soko.Unity.Game.Level.Management
         [Inject] private UiManager _uiManager;
         [Inject] private EventBus _eventBus;
         
-        public List<LevelPack> LevelPacks = new();
+        private List<LevelPack> _levelPacks = new();
+        
         public int LevelPackIndex { get; private set; }
         public int LevelIndex { get; private set; }
         public LevelPlayCycleManager PlayCycleManager { get; private set; }
 
-        public LevelsProgressSaveData SaveData => _progressSaveDataManager.SaveData;
+        private LevelsProgressSaveData SaveData => _progressSaveDataManager.SaveData;
         public LevelPack CurrentLevelPack => _levelPacksSo.LevelPacks[LevelPackIndex].LevelPack;
         public LevelData CurrentLevelData => CurrentLevelPack.Levels[LevelIndex];
         
@@ -37,9 +38,10 @@ namespace Soko.Unity.Game.Level.Management
         {
             FetchLevelPackDatas();
             CreateStartingDataIfNeeded();
+            RestoreIndexes();
         }
 
-        private void FetchLevelPackDatas() => LevelPacks = _levelPacksSo.LevelPacks.Select(so => so.LevelPack).ToList();
+        private void FetchLevelPackDatas() => _levelPacks = _levelPacksSo.LevelPacks.Select(so => so.LevelPack).ToList();
 
         private void CreateStartingDataIfNeeded()
         {
@@ -48,6 +50,12 @@ namespace Soko.Unity.Game.Level.Management
             SaveData.PackSaveDatas.Add(new ());
             SaveData.PackSaveDatas[0].Levels.Add(new ());
             _progressSaveDataManager.Save();
+        }
+
+        private void RestoreIndexes()
+        {
+            LevelPackIndex = SaveData.PackSaveDatas.Count - 1;
+            LevelIndex = SaveData.PackSaveDatas[LevelPackIndex].Levels.Count - 1;
         }
         
         public LevelState CheckLevelState(int packIndex, int levelIndex)
@@ -60,15 +68,15 @@ namespace Soko.Unity.Game.Level.Management
             if (levels.Count - 1 > levelIndex) return LevelState.Passed;
 
             // last pack and last level case
-            if (packIndex == SaveData.PackSaveDatas.Count &&
-                SaveData.PackSaveDatas.Count == LevelPacks.Count &&
-                levelIndex == SaveData.PackSaveDatas[packIndex].Levels.Count &&
+            if (packIndex == SaveData.PackSaveDatas.Count - 1 &&
+                SaveData.PackSaveDatas.Count == _levelPacks.Count &&
+                levelIndex == SaveData.PackSaveDatas[packIndex].Levels.Count - 1 &&
                 SaveData.PackSaveDatas[packIndex].Levels[levelIndex].BestTurnsCount != 0) return LevelState.Passed;
             
             return LevelState.Playable;
         }
         
-        // this soultion is 'kinda' weird, but also kinda consistens
+        // this soultion is 'kinda' weird, but also kinda consistent
         // we keep level stuff isolated inside and avoid DI crap
         public void SetCycleManager(LevelPlayCycleManager levelPlayCycleManager)
             => PlayCycleManager = levelPlayCycleManager;
@@ -106,7 +114,7 @@ namespace Soko.Unity.Game.Level.Management
                 LevelIndex++;
                 currentPackSaveData.Levels.Add(new ());
             }
-            else if (LevelPackIndex < LevelPacks.Count - 1)
+            else if (LevelPackIndex < _levelPacks.Count - 1)
             {
                 LevelPackIndex++;
                 LevelIndex = 0;
